df671d7e68eedc73e0da9f5ae670670eda8362fc
Paola De Nicola <denicola.paola@gmail.com>
2023-04-20 18:03:29 +0200

    primer_proyecto_premios_django

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    #donde vamos: /polls/5/
+    path("", views.IndexView.as_view(), name="index"),
+    #donde vamos: /polls/5/
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    #donde vamos: /polls/5/results
+    path("<int:pk>/results/", views.ResultView.as_view(), name="results"),
+    #donde vamos: /polls/5/vote
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
